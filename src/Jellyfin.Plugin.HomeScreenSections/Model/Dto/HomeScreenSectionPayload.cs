5b1eb83c44c068a5d2074f30388357901d90592d
BostonBella <161770033+BostonBella@users.noreply.github.com>
2026-04-27 20:44:55 +0000

    Remove stale src/ directory

--- src/Jellyfin.Plugin.HomeScreenSections/Model/Dto/HomeScreenSectionPayload.cs
+++
@@ -1,9 +0,0 @@
-namespace Jellyfin.Plugin.HomeScreenSections.Model.Dto
-{
-    public class HomeScreenSectionPayload
-    {
-        public Guid UserId { get; set; }
-
-        public string? AdditionalData { get; set; }
-    }
-}
--- src/Jellyfin.Plugin.HomeScreenSections/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using Jellyfin.Plugin.HomeScreenSections.Attributes;
-
-[assembly: AssemblyCompany("IAmParadox27")]
-[assembly: AssemblyProduct("Jellyfin.Plugin.HomeScreenSections")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyTitle("Jellyfin.Plugin.HomeScreenSections")]
-[assembly: AssemblyVersion("2.4.0.0")]
